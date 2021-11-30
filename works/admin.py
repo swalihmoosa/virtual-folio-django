625ec610852489bd102f15e2b9123ad9cdacb302
swalihmoosa <swalihmoosa72@gmail.com>
2021-11-30 10:09:26 +0530

    service model created

--- works/admin.py
+++ works/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from works.models import Service
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "image"]
+
+admin.site.register(Service, ServiceAdmin)
--- works/models.py
+++ works/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.fields.files import ImageField
 
-# Create your models here.
+
+class Service(models.Model):
+    image = models.FileField(upload_to="service/")
+    title = models.CharField(max_length=125)
+    description = models.TextField(max_length=355)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["id"]
